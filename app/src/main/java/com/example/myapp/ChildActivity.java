171ae33717cc486d01afb33362c447ae7942e906
manuelflrs <32782300+manuelflrs@users.noreply.github.com>
2020-08-27 20:10:59 -0500

    Funcionalidad semana 2 agregada

--- app/src/main/java/com/example/myapp/ChildActivity.java
+++ app/src/main/java/com/example/myapp/ChildActivity.java
@@ -2,13 +2,32 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChildActivity extends AppCompatActivity {
 
+    Button myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        myButton = findViewById(R.id.button2);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+
+                Intent myIntent = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(myIntent);
+
+            }
+        });
+
+
     }
 }
