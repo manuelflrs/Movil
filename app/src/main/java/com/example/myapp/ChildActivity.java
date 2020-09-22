54c5099402e2ac28b53d40f364505da667917b55
manuelflrs <32782300+manuelflrs@users.noreply.github.com>
2020-09-21 22:53:29 -0500

    Funcionalidad 3 agregada

--- app/src/main/java/com/example/myapp/ChildActivity.java
+++ app/src/main/java/com/example/myapp/ChildActivity.java
@@ -6,17 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
 
     Button myButton;
+    Intent i;
+    TextView myTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
-
+        i = getIntent();
+        String auxString;
+        auxString = i.getStringExtra("myParam");
+        myTextView = findViewById(R.id.textView1);
         myButton = findViewById(R.id.button2);
-
+        myTextView.setText(auxString);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,16 +11,18 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button myButton2;
     Button myButton;
     TextView myTextView;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         myButton = findViewById(R.id.button1);
+        myButton = findViewById(R.id.button3);
         myTextView = findViewById(R.id.textView1);
 
         myTextView.setText("Hello folks");
@@ -34,5 +37,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        myButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                myTextView.setText("You clicked with dialog");
+
+                Intent myIntent = new Intent(MainActivity.this, ChildActivity.class);
+                myIntent.putExtra("myParam", "Mensaje myParamm");
+                startActivity(myIntent);
+
+            }
+        });
+
     }
 }
