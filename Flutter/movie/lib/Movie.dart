c9823424c78857f587d185af14cf29b9a9a78539
manuelflrs <32782300+manuelflrs@users.noreply.github.com>
2020-10-29 19:32:46 -0600

    modified main added movie class

---
+++ Flutter/movie/lib/Movie.dart
@@ -0,0 +1,10 @@
+class Movie {
+  double popularity;
+  int voteCount;
+  bool video;
+  String posterPath;
+  int id;
+  bool adult;
+  String title;
+  double vote_average;
+}
--- Flutter/movie/lib/main.dart
+++ Flutter/movie/lib/main.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:movie/Home.dart';
 
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movies App',
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.yellow,
+          title: Text(
+            'Movies',
+            style: GoogleFonts.pacifico(
+              color: Colors.black,
+              fontSize: 30.0,
+            ),
+          ),
+        ),
+        body: Home(),
+      ),
+    );
+  }
+}
